c1d44ed2f220a735963fe4fea3045fc37b584266
yangja057 <yangja057@naver.com>
2017-06-19 13:42:09 +0900

    LocationAcitivity 재수정
    LocationActivity 수정 파일에서
    MainActivity 수정

--- app/src/main/java/com/example/yangj/en_0617/MainActivity.java
+++ app/src/main/java/com/example/yangj/en_0617/MainActivity.java
@@ -21,7 +21,7 @@ public class MainActivity extends AppCompatActivity {
         mRunnable = new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(getApplicationContext(), /*EIntroActivity.class*/LocationActivity.class);
+                Intent intent = new Intent(getApplicationContext(), EIntroActivity.class);
                 startActivity(intent);
             }
         };
